f80d0bb2d7dabbef052cbfd1f242840a946d1ea7
lnmrcloud <luis56009@gmail.com>
2024-02-01 08:37:15 -0600

    server go
    Prueba de creación de servidor go + pruebas de consumo sencillas

--- Tareas/Tarea1.md
+++ Tareas/Tarea1.md
@@ -1,1 +1,19 @@
 # Tarea 1
+
+## REST API
+
+### INSTALACION GO
+
++   https://go.dev/doc/install
++   Verificar instalación: $go version
+
+### CONFIGURACION GO
+
++   go mod init e
++   go get github.com/gin-gonic/gin
+
+
+## FRONT-END
+
+### INSTALACION REACT
+
--- Tareas/Tarea1/server-go/main.go
+++ Tareas/Tarea1/server-go/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct{
+	ID			string 			//json:id
+	Item 		string			//json:tittle
+	Completed 	bool			//json:completed
+}
+
+var todos = []todo{
+	{ID:"1", Item:"Clean Room" , Completed:false},
+	{ID:"2", Item:"Read book" , Completed:false},
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, todos)
+}
 
 func main() {
 	fmt.Println("This is my first program in Go")
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9000")
 }
