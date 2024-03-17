a79cfaa72c4629111896ad7d79e1fb38d106133d
lnmrcloud <luis56009@gmail.com>
2024-03-17 19:38:43 +0000

    frontend y backend docker compose
    frontend y backend docker compose

--- Proyecto1/backend/hello.go
+++ Proyecto1/backend/hello.go
@@ -14,7 +14,8 @@ import (
 const direcion = "/server-go"
 const ShellToUse = "cat"
 const ls = "ls"
-const comando = "ram_so1_1s2024"
+const comando_ram = "ram_so1_1s2024"
+const comando_cpu = "cpu_so1_1s2024"
 const meminfo = "meminfo"
 
 func get_ram_kernel(command string) (string, string, error) {
@@ -22,7 +23,22 @@ func get_ram_kernel(command string) (string, string, error) {
 	var stderr bytes.Buffer
 
 	//obtener dato de ram utilizada actual (se actualiza cada tiempo parametrizado)
-	cmd := exec.Command(ShellToUse, comando)
+	cmd := exec.Command(ShellToUse, comando_ram)
+	cmd.Dir = "/proc"
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+
+	return stdout.String(), stderr.String(), err
+
+}
+
+func get_cpu_kernel(command string) (string, string, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	//obtener dato de ram utilizada actual (se actualiza cada tiempo parametrizado)
+	cmd := exec.Command(ShellToUse, comando_cpu)
 	cmd.Dir = "/proc"
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -38,7 +54,7 @@ func main() {
 	// CONFIGURACION DE SOLICITUDES PARA USO DE BACKEND
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "http://frontend:80",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 	//HEALTHCHECK
@@ -48,6 +64,18 @@ func main() {
 	//OBTENER RAM DE KERNEL IMPORTADO CON VOLUMEN DESDE HOST UBUNTU 20.04
 	app.Get("/ram_kernel", func(c *fiber.Ctx) error {
 		out, errout, err := get_ram_kernel("ls -ltr")
+		if err != nil {
+			log.Printf("error: %v\n", err)
+		}
+		fmt.Println(out)
+		fmt.Println(errout)
+
+		return c.SendString(out)
+	})
+
+	//OBTENER PROCESOS CPU IMPORTADO CON VOLUMEN DESDE HOST UBUNTU 20.04
+	app.Get("/cpu_procesos_kernel", func(c *fiber.Ctx) error {
+		out, errout, err := get_cpu_kernel("ls -ltr")
 		if err != nil {
 			log.Printf("error: %v\n", err)
 		}
--- Proyecto1/frontend/proyecto1-app/src/App.js
+++ Proyecto1/frontend/proyecto1-app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {useRef} from 'react';
+import Axios from 'axios';
+
+var enviar_titulo
+var enviar_base64
 
 function App() {
+
+  const inputRef = useRef(null); 
+  const inputRef2 = useRef(null); 
+
+  //CONSULTAR LA CANTIDAD DE RAM EN USO ACTUAL
+  const handleClick = () => {
+    const respuesta = fetch("http://servergo:9000/ram_kernel")
+            .then(response => response.json())
+            .then(data => 
+              {
+                setButtonText(data)
+                })
+            .catch(error => console.error(error));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Proyecto 1 - Uso de recursos</h1>
+      <h2>RAM</h2>
+      <button onClick={handleClick}>Actualizar uso</button>
+      <br></br><br></br><br></br>
+      <h2>CPU</h2>
+
+      <br></br><br></br><br></br>
+      <h1>Proyecto 1 - Procesos CPU</h1>
+      <h2>Arbol de procesos</h2>
+      
+      <br></br><br></br><br></br>
     </div>
   );
 }
